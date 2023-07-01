2140c085fefd0d2e6c49ad44f2415f88487bb838
Sakshee1234 <sakshipatil4325@gmail.com>
2023-07-01 17:14:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,248 @@
+const WebSocket = require('ws');
+var id=0;
+console.log("hello");
+
+const players = [];
+
+var cards=["images/MsPatil.png","images/MsPathak.png","images/MsParmar.png","images/MsPawar.png",
+"images/rope.png","images/snipper.png","images/Wrench.png","images/leadpipe.png","images/knife.png",
+"images/candlestick.png","images/lounge.png","images/study.png","images/hall.png","images/playing.png",
+"images/kitchen.png","images/library.png"];
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
+  const rooms = new Map([
+    [1,"images/lounge"],
+    [2,"images/study" ],
+    [3,"images/hall" ],
+    [4,"images/playing" ],
+    [5,"images/kitchen" ],
+    [6,"images/library" ],
+  ]);
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+
+wss.on('connection', (ws) => {
+
+  ws.on('message', (message) => {
+    const data=JSON.parse(message);
+    if(data.type=="username"){
+        ws.id=id++;
+        players[ws.id]=ws;
+        
+        if(players.length>=3)
+        {
+            // wss.broadcast(JSON.stringify({type:"cards",data:2}));
+            shuffle(cards);
+            console.log(players[0].id);
+            players[0].send(JSON.stringify({data:3,card1:cards[0],card2:cards[1],card3:cards[2],card4:cards[3],card5:cards[4]}));
+            players[1].send(JSON.stringify({data:3,card1:cards[5],card2:cards[6],card3:cards[7],card4:cards[8],card5:cards[9]}));
+            players[2].send(JSON.stringify({data:3,card1:cards[10],card2:cards[11],card3:cards[12],card4:cards[13],card5:cards[14]}));
+            ans={};
+            ans[0]=cards[15];
+            ans[1]=cards[16];
+            ans[2]=cards[17];
+
+            player1cards=[];
+            player1cards[0]=cards[0];
+            player1cards[1]=cards[1];
+            player1cards[2]=cards[2];
+            player1cards[3]=cards[3];
+            player1cards[4]=cards[4];
+
+            player2cards=[];
+            player2cards[0]=cards[5];
+            player2cards[1]=cards[6];
+            player2cards[2]=cards[7];
+            player2cards[3]=cards[8];
+            player2cards[4]=cards[9];
+
+            player3cards=[];
+            player3cards[0]=cards[10];
+            player3cards[1]=cards[11];
+            player3cards[2]=cards[12];
+            player3cards[3]=cards[13];
+            player3cards[4]=cards[14];
+
+            
+            play();
+        }
+    }
+
+
+  });
+});
+
+function play()
+{
+
+    players[0].send(JSON.stringify({data:4}));
+    players[1].send(JSON.stringify({data:5}));
+    players[2].send(JSON.stringify({data:6}));
+
+    players[0].on('message', (message) => {
+       var temp=JSON.parse(message);
+       console.log(temp);
+       if(temp.type=="guess")
+       {
+          if(player2cards.includes(rooms.get(temp.data[0])) ||player2cards.includes(temp.data[1])|| player2cards.includes(temp.data[2]))
+          {
+            var t=[];
+            if(player2cards.includes(rooms.get(temp.data[0])))
+            t.push(rooms.get(temp.data[0]));
+            if(player2cards.includes(temp.data[1]))
+            t.push(temp.data[1]);
+            if(player2cards.includes(temp.data[2]))
+            t.push(temp.data[2]);
+            players[1].send(JSON.stringify({data:12,ans:t}));
+            players[1].on('message', (message2) => {
+              var counterguess=JSON.parse(message2);
+              if(counterguess.type=="counterguess")
+              {
+                players[0].send(JSON.stringify({data:122,ans:counterguess.data}));
+              }
+            })
+          }
+          else if(player3cards.includes(rooms.get(temp.data[0])) ||player3cards.includes(temp.data[1])|| player3cards.includes(temp.data[2]))
+          {
+            var t=[];
+            if(player3cards.includes(rooms.get(temp.data[0])))
+            t.push(rooms.get(temp.data[0]));
+            if(player3cards.includes(temp.data[1]))
+            t.push(temp.data[1]);
+            if(player3cards.includes(temp.data[2]))
+            t.push(temp.data[2]);
+            players[2].send(JSON.stringify({data:13,ans:t}));
+            players[2].on('message', (message2) => {
+              var counterguess=JSON.parse(message2);
+              if(counterguess.type=="counterguess")
+              {
+                players[0].send(JSON.stringify({data:133,ans:counterguess.data}));
+              }
+            })
+          }
+          else
+          {
+            players[0].send(JSON.stringify({data:100}));
+          }
+       }
+       else if(temp.type=="continue2")//player2 turn
+       {
+          players[1].send(JSON.stringify({data:4}));
+          players[2].send(JSON.stringify({data:5}));
+          players[0].send(JSON.stringify({data:6}));
+          players[1].on('message', (message2) => {
+            var temp2=JSON.parse(message2);
+            if(temp2.type=="guess"){
+              if(player1cards.includes(rooms.get(temp2.data[0])) || player1cards.includes(temp2.data[1]) || player1cards.includes(temp2.data[2]))
+              {
+                var t=[];
+                if(player1cards.includes(rooms.get(temp2.data[0])))
+                t.push(rooms.get(temp2.data[0]));
+                if(player1cards.includes(temp2.data[1]))
+                t.push(temp2.data[1]);
+                if(player2cards.includes(temp2.data[2]))
+                t.push(temp2.data[2]);
+                players[0].send(JSON.stringify({data:12,ans:t}));
+                players[0].on('message', (message2) => {
+                  var counterguess=JSON.parse(message2);
+                  if(counterguess.type=="counterguess")
+                  {
+                    players[1].send(JSON.stringify({data:122,ans:counterguess.data}));
+                  }
+                })
+              }
+              else if(player3cards.includes(rooms.get(temp2.data[0])) || player3cards.includes(temp2.data[1]) || player3cards.includes(temp2.data[2]))
+              {
+                var t=[];
+                if(player3cards.includes(rooms.get(temp2.data[0])))
+                t.push(rooms.get(temp2.data[0]));
+                if(player3cards.includes(temp2.data[1]))
+                t.push(temp2.data[1]);
+                if(player3cards.includes(temp2.data[2]))
+                t.push(temp2.data[2]);
+                players[2].send(JSON.stringify({data:13,ans:t}));
+                players[2].on('message', (message2) => {
+                  var counterguess=JSON.parse(message2);
+                  if(counterguess.type=="counterguess")
+                  {
+                    players[1].send(JSON.stringify({data:133,ans:counterguess.data}));
+                  }
+                })
+              }
+              else{
+                players[1].send(JSON.stringify({data:100}));
+              }
+
+            }
+            else if(temp2.type=="continue2")
+            {
+                players[2].send(JSON.stringify({data:4}));
+                players[0].send(JSON.stringify({data:5}));
+                players[1].send(JSON.stringify({data:6}));
+                players[2].on('message', (message3) => {
+                  var temp3=JSON.parse(message3);
+                  if(temp3.type=="guess"){
+                    if(player1cards.includes(rooms.get(temp3.data[0])) || player1cards.includes(temp3.data[1]) || player1cards.includes(temp3.data[2]))
+                    {
+                      var t=[];
+                      if(player1cards.includes(rooms.get(temp3.data[0])))
+                      t.push(rooms.get(temp3.data[0]));
+                      if(player1cards.includes(temp3.data[1]))
+                      t.push(temp3.data[1]);
+                      if(player1cards.includes(temp3.data[2]))
+                      t.push(temp3.data[2]);
+                      players[0].send(JSON.stringify({data:12,ans:t}));
+                      players[0].on('message', (message2) => {
+                        var counterguess=JSON.parse(message2);
+                        if(counterguess.type=="counterguess")
+                        {
+                          players[2].send(JSON.stringify({data:122,ans:counterguess.data}));
+                        }
+                      })
+                    }
+                    else if(player2cards.includes(rooms.get(temp3.data[0])) || player2cards.includes(temp3.data[1]) || player2cards.includes(temp3.data[2]))
+                    {
+                      var t=[];
+                      if(player2cards.includes(rooms.get(temp3.data[0])))
+                      t.push(rooms.get(temp3.data[0]));
+                      if(player2cards.includes(temp3.data[1]))
+                      t.push(temp3.data[1]);
+                      if(player2cards.includes(temp3.data[2]))
+                      t.push(temp3.data[2]);
+                      players[1].send(JSON.stringify({data:13,ans:t}));
+                      players[1].on('message', (message2) => {
+                        var counterguess=JSON.parse(message2);
+                        if(counterguess.type=="counterguess")
+                        {
+                          players[2].send(JSON.stringify({data:133,ans:counterguess.data}));
+                        }
+                      })
+                    }
+                    else{
+                      players[2].send(JSON.stringify({data:100}));
+                    }
+                  }
+                  else if(temp3.type=="continue2")
+                  {
+                    play();
+                  }
+                })
+            }
+          })
+
+       }
+    })
+}
+
---
+++ play.js
@@ -0,0 +1,312 @@
+const socket = new WebSocket('ws://localhost:8080');
+const uname = document.getElementById('username');
+const btn=document.getElementById('playbutton');
+var dies=["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
+var cards=["images/MsPatil.png","images/MsPathak.png","images/MsParmar.png","images/MsPawar.png",
+"images/dagger.png","images/Wrench.png","images/rope.png","images/knife.png","images/candlestick.png",
+"images/snipper.png","images/leadpipe.png","images/pistol.png","images/hall.png","images/playing.png",
+"images/kitchen.png","images/library.png"];
+var mycards=[]
+var r;
+var guess=[];
+const rooms = new Map([
+  [1,"lounge"],
+  [2,"study" ],
+  [3,"hall" ],
+  [4,"playing" ],
+  [5,"kitchen" ],
+  [6,"library" ],
+]);
+
+function weapon(){
+    const w1=document.getElementById("pcard5");
+    const w2=document.getElementById("pcard6");
+    const w3=document.getElementById("pcard7");
+    const w4=document.getElementById("pcard8");
+    const w5=document.getElementById("pcard9");
+    const w6=document.getElementById("pcard10");
+    const w7=document.getElementById("pcard11");
+    const w8=document.getElementById("pcard12");
+
+    w1.addEventListener('click',()=>{
+        guess.push(cards[4]);
+        w1.removeEventListener('click',w1);
+        w2.removeEventListener('click',w2);
+        w3.removeEventListener('click',w3);
+        w4.removeEventListener('click',w4);
+        w5.removeEventListener('click',w5);
+        w6.removeEventListener('click',w6);
+        w7.removeEventListener('click',w7);
+        w8.removeEventListener('click',w8);
+        socket.send(JSON.stringify({type:"guess",data:guess}));
+    })
+    w2.addEventListener('click',()=>{
+        guess.push(cards[5]);
+        w1.removeEventListener('click',w1);
+        w2.removeEventListener('click',w2);
+        w3.removeEventListener('click',w3);
+        w4.removeEventListener('click',w4);
+        w5.removeEventListener('click',w5);
+        w6.removeEventListener('click',w6);
+        w7.removeEventListener('click',w7);
+        w8.removeEventListener('click',w8);
+        socket.send(JSON.stringify({type:"guess",data:guess}));
+    })
+    w3.addEventListener('click',()=>{
+        guess.push(cards[6]);
+        w1.removeEventListener('click',w1);
+        w2.removeEventListener('click',w2);
+        w3.removeEventListener('click',w3);
+        w4.removeEventListener('click',w4);
+        w5.removeEventListener('click',w5);
+        w6.removeEventListener('click',w6);
+        w7.removeEventListener('click',w7);
+        w8.removeEventListener('click',w8);
+        socket.send(JSON.stringify({type:"guess",data:guess}));
+    })
+    w4.addEventListener('click',()=>{
+        guess.push(cards[7]);
+        w1.removeEventListener('click',w1);
+        w2.removeEventListener('click',w2);
+        w3.removeEventListener('click',w3);
+        w4.removeEventListener('click',w4);
+        w5.removeEventListener('click',w5);
+        w6.removeEventListener('click',w6);
+        w7.removeEventListener('click',w7);
+        w8.removeEventListener('click',w8);
+        socket.send(JSON.stringify({type:"guess",data:guess}));
+    })
+    w5.addEventListener('click',()=>{
+        guess.push(cards[8]);
+        w1.removeEventListener('click',w1);
+        w2.removeEventListener('click',w2);
+        w3.removeEventListener('click',w3);
+        w4.removeEventListener('click',w4);
+        w5.removeEventListener('click',w5);
+        w6.removeEventListener('click',w6);
+        w7.removeEventListener('click',w7);
+        w8.removeEventListener('click',w8);
+        socket.send(JSON.stringify({type:"guess",data:guess}));
+    })
+    w6.addEventListener('click',()=>{
+        guess.push(cards[9]);
+        w1.removeEventListener('click',w1);
+        w2.removeEventListener('click',w2);
+        w3.removeEventListener('click',w3);
+        w4.removeEventListener('click',w4);
+        w5.removeEventListener('click',w5);
+        w6.removeEventListener('click',w6);
+        w7.removeEventListener('click',w7);
+        w8.removeEventListener('click',w8);
+        socket.send(JSON.stringify({type:"guess",data:guess}));
+    })
+    w7.addEventListener('click',()=>{
+        guess.push(cards[10]);
+        w1.removeEventListener('click',w1);
+        w2.removeEventListener('click',w2);
+        w3.removeEventListener('click',w3);
+        w4.removeEventListener('click',w4);
+        w5.removeEventListener('click',w5);
+        w6.removeEventListener('click',w6);
+        w7.removeEventListener('click',w7);
+        w8.removeEventListener('click',w8);
+        socket.send(JSON.stringify({type:"guess",data:guess}));
+    })
+    w8.addEventListener('click',()=>{
+        guess.push(cards[11]);
+        w1.removeEventListener('click',w1);
+        w2.removeEventListener('click',w2);
+        w3.removeEventListener('click',w3);
+        w4.removeEventListener('click',w4);
+        w5.removeEventListener('click',w5);
+        w6.removeEventListener('click',w6);
+        w7.removeEventListener('click',w7);
+        w8.removeEventListener('click',w8);
+        socket.send(JSON.stringify({type:"guess",data:guess}));
+    })
+}
+function suspect(){
+    const s1=document.getElementById("pcard1");
+    const s2=document.getElementById("pcard2");
+    const s3=document.getElementById("pcard3");
+    const s4=document.getElementById("pcard4");
+
+    s1.addEventListener('click',()=>{
+        guess.push(cards[0]);
+        s1.removeEventListener('click',s1);
+        s2.removeEventListener('click',s2);
+        s3.removeEventListener('click',s3);
+        s4.removeEventListener('click',s4);
+        weapon();
+    })
+    s2.addEventListener('click',()=>{
+        guess.push(cards[1]);
+        s1.removeEventListener('click',s1);
+        s2.removeEventListener('click',s2);
+        s3.removeEventListener('click',s3);
+        s4.removeEventListener('click',s4);
+        weapon();
+    })
+    s3.addEventListener('click',()=>{
+        guess.push(cards[2]);
+        s1.removeEventListener('click',s1);
+        s2.removeEventListener('click',s2);
+        s3.removeEventListener('click',s3);
+        s4.removeEventListener('click',s4);
+        weapon();
+    })
+    s4.addEventListener('click',()=>{
+        guess.push(cards[3]);
+        s1.removeEventListener('click',s1);
+        s2.removeEventListener('click',s2);
+        s3.removeEventListener('click',s3);
+        s4.removeEventListener('click',s4);
+        weapon();
+    })
+}
+function guesscards(){
+    const g1=document.getElementById("cguess1");
+    const g2=document.getElementById("cguess2");
+    const g3=document.getElementById("cguess3");
+    g1.addEventListener('click',()=>{
+        socket.send(JSON.stringify({type:"counterguess",data:g1.src}))
+        g1.removeEventListener('click',g1);
+        document.getElementById("popup").style.display="none";
+    })
+    if(g2)
+    {
+        g2.addEventListener('click',()=>{
+            socket.send(JSON.stringify({type:"counterguess",data:g2.src}))
+            g2.removeEventListener('click',g2);
+            document.getElementById("popup").style.display="none";
+        })
+    }
+    if(g3)
+    {
+        g3.addEventListener('click',()=>{
+            socket.send(JSON.stringify({type:"counterguess",data:g3.src}))
+            g3.removeEventListener('click',g3);
+            document.getElementById("popup").style.display="none";
+        })
+    }
+}
+socket.addEventListener('message', (event) => {
+  var m = JSON.parse(event.data);
+  console.log(m);
+  if(m.data==3)
+  {
+    document.getElementById("beforeplaying").style.display="none";
+    console.log("received");
+    document.getElementById("card1").src=m.card1;
+    document.getElementById("card2").src=m.card2;
+    document.getElementById("card3").src=m.card3;
+    document.getElementById("card4").src=m.card4;
+    document.getElementById("card5").src=m.card5;
+    mycards.push(m.card1);
+    mycards.push(m.card2);
+    mycards.push(m.card3);
+    mycards.push(m.card4);
+    mycards.push(m.card5);
+
+  }
+  else if(m.data==4)
+  {
+    document.getElementById("gamemessages").innerText="Your turn, roll the die";
+    document.getElementById("dieimage").addEventListener('click',()=>{
+        r=Math.floor(Math.random()*6);
+        const el=document.getElementById("dieimage");
+        if(el){
+            el.src=dies[r];
+        }
+        guess.push(r);
+        el.removeEventListener('click',el);
+        document.getElementById("gamemessages").innerText="You have entered "+rooms.get(r)+" now choose the suspect and suspicious weapon";
+        suspect();
+    });
+  }
+  else if(m.data==5 || m.data==6)
+  {
+    document.getElementById("gamemessages").innerText="Player is playing";
+  }
+  else if(m.data==12 || m.data==13)
+  {
+    const pop=document.getElementById("popup");
+    document.getElementById("pmsg").innerHTML="Player 1 has made a guess";
+    console.log(m.ans[0]);
+    document.getElementById("cguess1").src=m.ans[0];
+    if(m.ans.length==2)
+    {
+        const box=document.createElement("div");
+        box.id="guess2";
+        document.getElementById("cardguess").appendChild(box);
+        const img=document.createElement("img");
+        img.id="cguess2";
+        img.src=m.ans[1];
+        img.style.width="50px";
+        img.style.height="150px";
+        document.getElementById("guess2").appendChild(img);
+    }
+    else if(m.ans.length==3)
+    {
+        const box1=document.createElement("div");
+        box1.id="guess2";
+        document.getElementById("cardguess").appendChild(box1);
+        const img1=document.createElement("img");
+        img1.id="cguess2";
+        img1.src=m.ans[1];
+        img1.style.width="50px";
+        img1.style.height="150px";
+        document.getElementById("guess2").appendChild(img1);
+
+        const box2=document.createElement("div");
+        box2.id="guess3";
+        document.getElementById("cardguess").appendChild(box2);
+        const img2=document.createElement("img");
+        img2.id="cguess3";
+        img2.src=m.ans[2];
+        img2.style.width="50px";
+        img2.style.height="150px";
+        document.getElementById("guess3").appendChild(img2);
+    }
+    pop.style.display="block";
+    guesscards();
+  }
+  if(m.data==122 || m.data==133)
+  {
+    const pop=document.getElementById("popup");
+    document.getElementById("pmsg").innerHTML="Someone has your guess ";
+    document.getElementById("cguess1").src=m.ans;
+    pop.style.display="block";
+
+    document.getElementById("cguess1").addEventListener('click',()=>{
+        socket.send(JSON.stringify({type:"continue2"}))
+        document.getElementById("popup").style.display="none";
+    })
+  }
+  if(m.data==100)
+  {
+    document.getElementById("gamemessages").innerText="You have guessed it correct game ends";
+  }
+});
+
+
+function sendMessage() {
+  const messageInput = document.getElementById('messageInput');
+  const message = messageInput.value;
+  socket.send(JSON.stringify({ type:"username",data:message }));
+  messageInput.value = '';
+}
+
+function playbtn()
+{
+    console.log("clicked");
+    if(uname.value=="")
+    {
+      alert("Please enter a name");
+      return;
+    }
+    const user = uname.value;
+    socket.send(JSON.stringify({ type:"username",data:user }));
+    uname.value = '';
+    alert('Hi '+user+' waiting for other players to join');
+}
